d102924dc6c0b7e560430480e7e76e61480300a8
bangexiexing <qiuyunlong0403@qq.com>
2018-06-19 21:13:32 +0800

    redis lock

--- src/main/java/bucket/component/redis/RedisLock.java
+++ src/main/java/bucket/component/redis/RedisLock.java
@@ -3,6 +3,8 @@ package bucket.component.redis;
 import bucket.component.SpringApplicationContextHolder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.core.BoundGeoOperations;
+import org.springframework.data.redis.core.BoundValueOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 
 import java.time.Instant;
@@ -24,18 +26,25 @@ public class RedisLock implements Lock {
     private RedisTemplate redisTemplate;
     private String key;
     private volatile boolean locked = false;
+    private RetryStrategy retryStrategy;
 
     private RedisLock(){}
 
-    private RedisLock(String key){
+    public RedisLock(String key){
         this.key = key;
         this.redisTemplate = SpringApplicationContextHolder.getBean(RedisTemplate.class);
+        this.retryStrategy = new LockIfAbsent();
     }
 
-    private RedisLock(String key,int expiredSecond){
+    public RedisLock(String key,int expiredSecond){
         this.key = key;
         this.EXPIRED_SECOND = expiredSecond;
         this.redisTemplate = SpringApplicationContextHolder.getBean(RedisTemplate.class);
+        this.retryStrategy = new LockIfAbsent();
+    }
+
+    public void useLockIfExpiredStrategy(){
+        this.retryStrategy = new LockIfExpired();
     }
 
     public String getKey() {
@@ -46,20 +55,70 @@ public class RedisLock implements Lock {
         return locked;
     }
 
+    interface RetryStrategy{
+        boolean retry();
+    }
+    //只有对方释放锁后才获取锁，超时抛异常
+    class LockIfAbsent implements RetryStrategy{
+        @Override
+        public boolean retry() {
+            long start = System.currentTimeMillis();
+            final Random r = new Random();
+            int time_out = EXPIRED_SECOND * 1000;
+            Long value = (Long) redisTemplate.opsForValue().get(key);
+            //maybe unnecessary
+            if (value != null && (System.currentTimeMillis() - value > time_out)){
+                throw new RuntimeException(Thread.currentThread().getName()+" get redis lock time out, key is :" + key);
+            }
+            //start = value == null ? start : value;
+            while (!tryLock()){
+                if (System.currentTimeMillis() - start > time_out){
+                    throw new RuntimeException(Thread.currentThread().getName()+" get redis lock time out, key is :" + key);
+                }
+                try {
+                    Thread.sleep(10, r.nextInt(1000));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            return true;
+        }
+    }
+
+    //对方锁持有时间超时后，尝试获取锁。
+    class LockIfExpired implements RetryStrategy{
+        @Override
+        public boolean retry() {
+            final Random r = new Random();
+            int time_out = EXPIRED_SECOND * 1000;
+            BoundValueOperations valueOps = redisTemplate.boundValueOps(key);
+            while (!tryLock()){
+                Long value = (Long) valueOps.getKey();
+                if (value == null) continue;
+                if (System.currentTimeMillis() - value > time_out){
+                    Long oldValue = (Long) valueOps.getAndSet(System.currentTimeMillis());
+                    //如果是空，表示锁已经被释放
+                    //oldValue != value 表示已经有人先一步获取了锁
+                    if (oldValue == null || oldValue == value) {
+                        locked = true;
+                        break;
+                    }
+                }else {
+                    try {
+                        Thread.sleep(10, r.nextInt(1000));
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            return true;
+        }
+    }
+
     @Override
     public void lock() {
-        long start = System.currentTimeMillis();
-        final Random r = new Random();
-        int time_out = EXPIRED_SECOND * 1000;
-        while (!tryLock()){
-            if(System.currentTimeMillis() - start > time_out){
-                throw new RuntimeException(Thread.currentThread().getName()+" get redis lock time out,key is :"+key);
-            }
-            try {
-                Thread.sleep(10, r.nextInt(1000));
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+        if (!tryLock()){
+            retryStrategy.retry();
         }
     }
 
@@ -70,8 +129,8 @@ public class RedisLock implements Lock {
 
     @Override
     public boolean tryLock() {
-        locked = redisTemplate.opsForValue().setIfAbsent(key,Instant.now().getEpochSecond());
-        //确保时间内业务能完成
+        locked = redisTemplate.opsForValue().setIfAbsent(key,System.currentTimeMillis());
+        //确保时间内业务能完成,或许应该不设过期时间，交给持有者释放
         redisTemplate.expire(key,EXPIRED_SECOND,TimeUnit.SECONDS);
         return locked;
     }
